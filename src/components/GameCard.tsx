1993ba550a4702841c290eca5d463ed28c5da672
yoe7501 <yoel7501@gmail.com>
2024-10-01 15:36:11 -0400

    refactor remove duplicated styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,5 @@
-
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Image, Heading,  HStack } from "@chakra-ui/react";
+import { Card, CardBody, Image, Heading, HStack } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
@@ -11,18 +10,18 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card width='300px' borderRadius={10} overflow="hidden">
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent='space-between'>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-      </CardBody>
-    </Card>
+      <Card width="300px" borderRadius={10} overflow="hidden">
+        <Image src={getCroppedImageUrl(game.background_image)} />
+        <CardBody>
+          <Heading fontSize="2xl">{game.name}</Heading>
+          <HStack justifyContent="space-between">
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+        </CardBody>
+      </Card>
   );
 };
 
--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,13 +1,13 @@
-import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react'
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 const GameCardSkeleton = () => {
   return (
-    <Card width = '300px' borderRadius={10} overflow="hidden">
-        <Skeleton height="200px"/>
+      <Card width="300px" borderRadius={10} overflow="hidden">
+        <Skeleton height="200px" />
         <CardBody>
-            <SkeletonText/>
+          <SkeletonText />
         </CardBody>
-    </Card>
-  )
-}
+      </Card>
+  );
+};
 
-export default GameCardSkeleton
+export default GameCardSkeleton;
