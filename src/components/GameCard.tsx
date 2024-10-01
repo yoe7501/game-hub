c7f6fd3d5a59802b00acb3d0b9ba753de9410539
yoe7501 <yoel7501@gmail.com>
2024-10-01 15:04:34 -0400

    Meta Critic Implementation

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import { Game } from '../hooks/useGames'
-import { Card, CardBody, Image, Heading } from '@chakra-ui/react'
+import React from "react";
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, Image, Heading, Text, HStack } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
-    game: Game
+  game: Game;
 }
 
-const GameCard = ({game}: Props) => {
+const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow='hidden'>
-        <Image src={game.background_image}/>
-        <CardBody>
-            <Heading fontSize='2xl'>{game.name}</Heading>
-        </CardBody>
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <HStack justifyContent='space-between'>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+      </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
