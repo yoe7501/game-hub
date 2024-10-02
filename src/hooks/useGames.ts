fbceafe88ddec349b27d682ddccf332b554606d2
yoe7501 <yoel7501@gmail.com>
2024-10-02 14:43:08 -0400

    Sort by

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -20,7 +20,7 @@ const useGames = (
 ) =>
   useData<Game>(
     "/games",
-    { params: { genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id } },
+    { params: { genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder } },
     [gameQuery]
   );
 
