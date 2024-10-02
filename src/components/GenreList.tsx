f12a768883936bfa26b6e6e4f9beaa49a59bfc35
yoe7501 <yoel7501@gmail.com>
2024-10-01 22:39:19 -0400

    Set selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,14 @@
-import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import { HStack, List, ListItem, Image, Text, Button } from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 import GenreSkeleton from "./GenreSkeleton";
-const GenreList = () => {
+
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
+}
+
+const GenreList = ({selectedGenre, onSelectGenre} : Props ) => {
   const { data, isLoading, error } = useGenres();
   const skeletons = [1 , 2, 3 , 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16];
 
@@ -10,20 +16,20 @@ const GenreList = () => {
   return (
     <List>
         {isLoading && skeletons.map((skeleton)=> (
-            <ListItem   paddingY='10px'>
-                     <GenreSkeleton key={skeleton}/>
+            <ListItem  key={skeleton} paddingY='10px'>
+                     <GenreSkeleton />
             </ListItem>
            
         ))}
       {data.map((genre) => (
-        <ListItem key={genre.id} paddingY='5px'>
+        <ListItem key={genre.id} paddingY='5px' borderRadius='10px' backgroundColor={genre.id === selectedGenre?.id ? 'gray.900' : 'normal'}>
           <HStack>
             <Image
               boxSize="32px"
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             />
-            <Text fontSize='lg'>{genre.name}</Text>
+            <Button  onClick={() => onSelectGenre(genre)}variant='link' fontSize='lg'>{genre.name}</Button>
           </HStack>
         </ListItem>
       ))}
